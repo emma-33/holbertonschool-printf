7569468f47799527714077fd9ac712fdad513cf9
mamuche <7558@holbertonstudents.com>
2023-11-21 06:02:21 -0800

    added pointer to char_num

--- print_c_and_s.c
+++ print_c_and_s.c
@@ -11,7 +11,7 @@ void print_char(va_list ap, int *char_num)
 	char c = va_arg(ap, int);
 
 	write(1, &c, 1);
-	char_num++;
+	*char_num++;
 }
 
 
@@ -34,7 +34,7 @@ void print_str(va_list ap, int *char_num)
 	while (str[str_len] != '\0')
 		str_len++;
 	write(1, str, str_len);
-	char_num += str_len;
+	*char_num += str_len;
 
 }
 
@@ -50,6 +50,6 @@ void print_prct(va_list ap, int *char_num)
 	char prct = va_arg(ap, int);
 
 	write(1, &prct, 1);
-	char_num++;
+	*char_num++;
 
 }
