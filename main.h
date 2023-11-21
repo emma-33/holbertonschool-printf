55ae9cb00fc2b9f232e8ca45d4c05f67c7326686
mamuche <7558@holbertonstudents.com>
2023-11-21 02:49:01 -0800

    added print_prct, del struct

--- main.h
+++ main.h
@@ -8,25 +8,10 @@
 #include <stdarg.h>
 
 
-/**
- *
- * struct choice - parameter to check and print.
- * @letter: type.
- * @print: fonction to send to.
- *
- */
-
-typedef struct choice
-{
-	char letter;
-	int (*print)(va_list ap);
-} choice;
-
-
-
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list ap);
 int print_str(va_list ap);
+int print_prct(va_list ap);
 
 #endif
