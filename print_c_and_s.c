f9aeadda8b4332c0f7a929283ac1393e0a4532c2
mamuche <7558@holbertonstudents.com>
2023-11-21 05:34:05 -0800

    remove return

--- print_c_and_s.c
+++ print_c_and_s.c
@@ -1,33 +1,27 @@
 #include "main.h"
 
 /**
- *
  * print_char - print character.
  * @ap: list.
  * @char_num: counter for character.
- *
  */
 
-int print_char(va_list ap, int *char_num)
+void print_char(va_list ap, int *char_num)
 {
 	char c = va_arg(ap, int);
 
 	write(1, &c, 1);
 	char_num++;
-
-	return(1);
 }
 
 
 /**
- *
  * print_str - print string.
  * @ap: list.
  * @char_num: counter for character.
- *
  */
 
-int print_str(va_list ap, int *char_num)
+void print_str(va_list ap, int *char_num)
 {
 	char *str;
 	int str_len = 0;
@@ -42,24 +36,20 @@ int print_str(va_list ap, int *char_num)
 	write(1, str, str_len);
 	char_num += str_len;
 
-	return(1);
 }
 
 
 /**
- *
  * print_prct - print pourcentage.
  * @ap: list.
  * @char_num: counter for characters.
- *
  */
 
-int print_prct(va_list ap, int *char_num)
+void print_prct(va_list ap, int *char_num)
 {
 	char prct = va_arg(ap, int);
 
 	write(1, &prct, 1);
 	char_num++;
 
-	return(1);
 }
