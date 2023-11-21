148e95ec19a58f61989ad68578fcccbeddd3e219
emma-33 <emma.lateyron@orange.fr>
2023-11-21 14:53:08 +0100

    test 2

--- _printf.c
+++ _printf.c
@@ -9,9 +9,6 @@ int _printf(const char *format, ...)
 {
 	int i = 0;
 	int char_num = 0;
-	int (*p_p)(va_list, int) = &print_prct;
-	int (*p_c)(va_list, int) = &print_char;
-	int (*p_s)(va_list, int) = &print_str;
 
 	/*char *sep = "";*/
 	va_list ap;
@@ -32,13 +29,13 @@ int _printf(const char *format, ...)
 				break;
 
 			if (format[i] == '%')
-				(*p_p)(ap, char_num);
+				print_prct(ap, &char_num);
 
 			else if (format[i] == 'c')
-				(*p_c)(ap, char_num);
+				print_char(ap, &char_num);
 
 			else if (format[i] == 's')
-				(*p_s)(ap, char_num);
+				print_str(ap, &char_num);
 		}
 		format++;
 	}
