1b08c07f7dee414ab5420c691dcfd1a517c6eafa
emma-33 <emma.lateyron@orange.fr>
2023-11-23 12:25:34 +0100

    fixed betty

--- _printf.c
+++ _printf.c
@@ -13,41 +13,37 @@ int _printf(const char *format, ...)
 	int (*f)(va_list);
 
 	if (format == NULL)
-	{
 		return (-1);
-	}
 	va_start(ap, format);
 
 	while (format[i] != '\0')
 	{
-			if (format[i] == '%' && (!format[i + 1] || format[i + 1] == '\0'))
-			{
-				return (-1);
-			}
-			if (format[i] == '%')
-			{
-				f = get_function(format[i + 1]);
+		if (format[i] == '%' && (!format[i + 1] || format[i + 1] == '\0'))
+			return (-1);
+		if (format[i] == '%')
+		{
+			f = get_function(format[i + 1]);
 
-				if (f != NULL)
-				{
-					char_count += f(ap);
-					i++;
-				}
-				else
-				{
-					_putchar(format[i]);
-					_putchar(format[i + 1]);
-					i++;
-					char_count += 2;
-				}
+			if (f != NULL)
+			{
+				char_count += f(ap);
+				i++;
 			}
 			else
 			{
 				_putchar(format[i]);
-				char_count++;
+				_putchar(format[i + 1]);
+				i++;
+				char_count += 2;
 			}
-			i++;
 		}
+		else
+		{
+			_putchar(format[i]);
+			char_count++;
+		}
+		i++;
+	}
 	va_end(ap);
 	return (char_count);
 }
